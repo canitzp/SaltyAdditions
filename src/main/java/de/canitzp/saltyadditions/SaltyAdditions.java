0ae4c764b165648934665ffa5de1a0a5ad6bae17
canitzp <canitzp@googlemail.com>
2016-01-24 12:09:04 +0100

    1.8.9

--- src/main/java/de/canitzp/saltyadditions/SaltyAdditions.java
+++ src/main/java/de/canitzp/saltyadditions/SaltyAdditions.java
@@ -1,31 +1,43 @@
 package de.canitzp.saltyadditions;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import de.ellpeck.actuallyadditions.mod.items.metalists.TheFoods;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 /**
  * @author canitzp
  * @version 16.02.1
  */
 @SuppressWarnings("unchecked")
-@Mod(modid = SaltyAdditions.MODID, name = SaltyAdditions.NAME, version = SaltyAdditions.VERSION, dependencies = "required-after:ActuallyAdditions;required-after:SaltMod@[1.7,)")
+@Mod(modid = SaltyAdditions.MODID, name = SaltyAdditions.NAME, version = SaltyAdditions.VERSION, dependencies = "required-after:ActuallyAdditions@[r24,);required-after:SaltMod@[1.7,)")
 public class SaltyAdditions {
 
     public static final String MODID = "saltyadditions";
     public static final String NAME = "SaltyAdditions";
-    public static final String VERSION = "R-1.7.10-16.02.1";
+    public static final String VERSION = "@VERSION";
     public static final String UNLOCALIZEDNAME = MODID + ".";
+    public static Logger logger = LogManager.getLogger(SaltyAdditions.NAME);
+    private SaltyFood[] foodArray;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         TheFoods[] allFoods = TheFoods.values();
+        foodArray = new SaltyFood[allFoods.length];
         for (TheFoods allFood : allFoods) {
-            new SaltyFood(allFood);
+            foodArray[allFood.ordinal()] = new SaltyFood(allFood);
         }
-        Logger logger = LogManager.getLogger(SaltyAdditions.NAME);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        for (SaltyFood food : foodArray) {
+            food.init();
+        }
         logger.info("SaltyAdditions loading completed!");
     }
 
--- src/main/java/de/canitzp/saltyadditions/SaltyFood.java
+++ src/main/java/de/canitzp/saltyadditions/SaltyFood.java
@@ -1,22 +1,20 @@
 package de.canitzp.saltyadditions;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import de.ellpeck.actuallyadditions.mod.items.InitItems;
 import de.ellpeck.actuallyadditions.mod.items.metalists.TheFoods;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
 import net.minecraft.util.StatCollector;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import ru.liahim.saltmod.common.CommonProxy;
 import ru.liahim.saltmod.common.ModItems;
 
 public class SaltyFood extends ItemFood{
 
-    private TheFoods item;
-    private IIcon[] textures;
+    public TheFoods item;
+    //private IIcon[] textures;
 
     public SaltyFood(TheFoods foods){
         super(foods.healAmount + 2, foods.saturation + 2F, false);
@@ -27,6 +25,10 @@ public class SaltyFood extends ItemFood{
         GameRegistry.addShapelessRecipe(new ItemStack(this), new ItemStack(ModItems.saltPinch), new ItemStack(InitItems.itemFoods, 1, foods.ordinal()));
     }
 
+    public void init(){
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(this, item.ordinal(), new ModelResourceLocation(SaltyAdditions.MODID + ":saltyFood", "inventory"));
+    }
+/*
     @Override
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister reg) {
@@ -53,7 +55,7 @@ public class SaltyFood extends ItemFood{
     public boolean requiresMultipleRenderPasses() {
         return true;
     }
-
+*/
     @Override
     public String getItemStackDisplayName(ItemStack stack) {
         return StatCollector.translateToLocal(SaltyAdditions.MODID + ".salty.name") + " " + StatCollector.translateToLocal("item.actuallyadditions.itemFood" + item.name + ".name");
